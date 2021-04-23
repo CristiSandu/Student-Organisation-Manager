94d9069abd1e887176b89ae6d99da8b38800959b
cristysandu <crystisandu3@gmail.com>
2021-04-23 16:53:58 +0300

    set IOS firebase

---
+++ StudentOrganisation/StudentOrganisation/Services/IFirebaseAuthentication.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentOrganisation.Services
+{
+    public interface IFirebaseAuthentication
+    {
+        Task<string> LoginWithEmailAndPassword(string email, string password);
+        bool SignOut();
+        bool IsSignIn();
+    }
+}
