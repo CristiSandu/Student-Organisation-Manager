5261037c5fb4055a78bf31ce752eb7ba8683cb91
cristysandu <crystisandu3@gmail.com>
2021-04-23 16:45:09 +0300

    add firebase on Android

--- StudentOrganisation/StudentOrganisation.Android/MainActivity.cs
+++ StudentOrganisation/StudentOrganisation.Android/MainActivity.cs
@@ -4,6 +4,7 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Firebase;
 
 namespace StudentOrganisation.Droid
 {
@@ -16,6 +17,7 @@ namespace StudentOrganisation.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            FirebaseApp.InitializeApp(Application.Context);
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
