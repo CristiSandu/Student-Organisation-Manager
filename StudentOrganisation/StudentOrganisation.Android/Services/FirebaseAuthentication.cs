c7bab47a87dad78b8e97ef8229bc55885f049302
cristysandu <crystisandu3@gmail.com>
2021-04-23 17:09:41 +0300

    set authentification service

---
+++ StudentOrganisation/StudentOrganisation.Android/Services/FirebaseAuthentication.cs
@@ -0,0 +1,59 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Gms.Extensions;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Firebase.Auth;
+using StudentOrganisation.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentOrganisation.Droid.Services
+{
+    class FirebaseAuthentication : IFirebaseAuthentication
+    {
+        public bool IsSignIn()
+        {
+            var user = FirebaseAuth.Instance.CurrentUser;
+            return user != null;
+        }
+
+        public async Task<string> LoginWithEmailAndPassword(string email, string password)
+        {
+            try
+            {
+                var user = await FirebaseAuth.Instance.SignInWithEmailAndPasswordAsync(email, password);
+                var token = await user.User.GetIdToken(false);
+                return token.ToString();
+            }
+            catch (FirebaseAuthInvalidUserException e)
+            {
+                e.PrintStackTrace();
+                return string.Empty;
+            }
+            catch (FirebaseAuthInvalidCredentialsException e)
+            {
+                e.PrintStackTrace();
+                return string.Empty;
+            }
+        }
+
+        public bool SignOut()
+        {
+            try
+            {
+                FirebaseAuth.Instance.SignOut();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ StudentOrganisation/StudentOrganisation.iOS/Services/FirebaseAuthentication.cs
@@ -0,0 +1,40 @@
+﻿using Firebase.Auth;
+using Foundation;
+using StudentOrganisation.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UIKit;
+
+namespace StudentOrganisation.iOS.Services
+{
+    public class FirebaseAuthentication : IFirebaseAuthentication
+    {
+        public bool IsSignIn()
+        {
+            var user = Auth.DefaultInstance.CurrentUser;
+            return user != null;
+        }
+
+        public async Task<string> LoginWithEmailAndPassword(string email, string password)
+        {
+            var user = await Auth.DefaultInstance.SignInWithPasswordAsync(email, password);
+            return await user.User.GetIdTokenAsync();
+        }
+
+        public bool SignOut()
+        {
+            try
+            {
+                _ = Auth.DefaultInstance.SignOut(out NSError error);
+                return error == null;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
