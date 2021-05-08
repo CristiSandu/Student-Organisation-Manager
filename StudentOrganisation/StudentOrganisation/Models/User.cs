b943cf3c177956056d6e79db1592d4121f26012d
Adrian Margineanu <a.margineanu98@gmail.com>
2021-05-09 00:00:37 +0300

    Added the required datails for  the user fields

--- StudentOrganisation/StudentOrganisation/Models/User.cs
+++ StudentOrganisation/StudentOrganisation/Models/User.cs
@@ -1,21 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using Plugin.CloudFirestore.Attributes;
 using System.Text;
 
 namespace StudentOrganisation.Models
 {
     public class User
     {
-        public enum Pozition {Junior = 0, Member = 1, Mentor= 2, Admin=3}; 
+        public enum Pozition {Junior = 0, Member = 1, Mentor= 2, Admin=3};
+
+        public static string CollectionPath = "users";
+        [Id]
         public string Id { get; set; }
+        [MapTo("name")]
         public string Name { get; set; }
+        [MapTo("second_name")]
         public string SecondName { get; set; }
+        [MapTo("email")]
         public string Email { get; set; }
+        [MapTo("picture_url")]
         public string PictureURL { get; set; }
+        [MapTo("path")]
         public string Path { get; set; }
+        [MapTo("stars")]
         public int Stars { get; set; }
+        [MapTo("role")]
         private int Role { get; set; }
+        [MapTo("highlists")]
         public List<string> Highlits {get; set;}
+        [MapTo("is_present")]
         public bool IsPresent { get; set; }
 
         public bool IsAdmin => Role > 2;
