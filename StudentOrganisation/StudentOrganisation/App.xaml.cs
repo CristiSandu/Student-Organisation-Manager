c36270ad38d41d49080e8fb3275bfbc55c734433
cristysandu <crystisandu3@gmail.com>
2021-05-09 21:49:28 +0300

    Small changes to Display

--- StudentOrganisation/StudentOrganisation/App.xaml.cs
+++ StudentOrganisation/StudentOrganisation/App.xaml.cs
@@ -20,7 +20,7 @@ namespace StudentOrganisation
             }
             else
             {
-                MainPage = new TestLogin();
+                MainPage = new AppShell();
             }
         }
 
