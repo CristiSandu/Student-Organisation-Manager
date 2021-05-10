e586b63e969d805dea26f3fafda14399bf12107e
Adrian Margineanu <a.margineanu98@gmail.com>
2021-05-10 18:46:47 +0300

    Added Links in the db. Add methods to create, get and update the links.

--- StudentOrganisation/StudentOrganisation/Models/LinksModel.cs
+++ StudentOrganisation/StudentOrganisation/Models/LinksModel.cs
@@ -1,13 +1,18 @@
-﻿using System;
+﻿using Plugin.CloudFirestore.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace StudentOrganisation.Models
 {
-    class LinksModel
+    public class LinksModel
     {
+        public static string CollectionPath = "websites"
+        [MapTo("title")]
         public string Title { get; set; }
+        [MapTo("url")]
         public string URL { get; set; }
+        [MapTo("date")]
         public DateTime Date { get; set; }
     }
 }
---
+++ StudentOrganisation/StudentOrganisation/Services/LinksProvider.cs
@@ -0,0 +1,39 @@
+﻿using Plugin.CloudFirestore;
+using StudentOrganisation.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StudentOrganisation.Services
+{
+    public class LinksProvider
+    {
+        private static IFirestore _cloud = CrossCloudFirestore.Current.Instance;
+        public static async Task<bool> Create(LinksModel link)
+        {
+            await _cloud.Collection(LinksModel.CollectionPath)
+                        .Document(link.Title.Replace(" ", "_"))
+                        .SetAsync(link);
+            return true;
+        }
+
+        public static async Task<List<LinksModel>> GetAll()
+        {
+            IQuerySnapshot query = await _cloud
+                                    .Collection(LinksModel.CollectionPath)
+                                    .OrderBy("title")
+                                    .GetAsync();
+
+            IEnumerable<LinksModel> links = query.ToObjects<LinksModel>();
+            return new List<LinksModel>(links);
+        }
+
+        public static async Task<bool> Update(LinksModel link)
+        {
+            await _cloud.Collection(LinksModel.CollectionPath)
+                        .Document(link.Title.Replace(" ", "_"))
+                        .UpdateAsync(link);
+
+            return true;
+        }
+    }
+}
