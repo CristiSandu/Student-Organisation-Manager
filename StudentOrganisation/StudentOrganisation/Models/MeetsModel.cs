13264e1fb1464ccd171d5e243638f72a591d9ca8
Adrian Margineanu <a.margineanu98@gmail.com>
2021-05-10 19:14:52 +0300

    Add Meets model in db

---
+++ StudentOrganisation/StudentOrganisation/Models/MeetsModel.cs
@@ -0,0 +1,26 @@
+﻿using Plugin.CloudFirestore.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentOrganisation.Models
+{
+    public class MeetsModel
+    {
+        public static string CollectionPath = "meets";
+        [MapTo("title")]
+        public string Title { get; set; }
+        [MapTo("description")]
+        public string Description { get; set; }
+        [MapTo("role")]
+        public int Role { get; set; }
+        [MapTo("location")]
+        public string Location { get; set; }
+        [MapTo("link")]
+        public string link { get; set; }
+        [MapTo("attendance_list")]
+        public List<string> AttendanceList { get; set; }
+        [MapTo("date")]
+        public DateTime Date { get; set; }
+    }
+}
