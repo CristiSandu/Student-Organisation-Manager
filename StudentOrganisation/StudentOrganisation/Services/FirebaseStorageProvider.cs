6163ad013cbb3d5b69c0f810d762fa960c155e11
Adrian Margineanu <a.margineanu98@gmail.com>
2021-05-10 16:33:36 +0300

    Added link to  firebase storage. Added profile picture upload and download

---
+++ StudentOrganisation/StudentOrganisation/Services/FirebaseStorageProvider.cs
@@ -0,0 +1,47 @@
+﻿using Firebase.Storage;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentOrganisation.Services
+{
+    public class FirebaseStorageProvider
+    {
+        public static async Task<string> StoreProfilePicture(Stream image, Models.User user)
+        {
+            try
+            {
+                string url = await new FirebaseStorage("studentorganisati.appspot.com")
+                    .Child("user")
+                    .Child(user.Id)
+                    .Child("profile.jpg")
+                    .PutAsync(image);
+                return url;
+            }catch(Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+        }
+
+        public static async Task<string> GetProfilePicture(Models.User user)
+        {
+            try
+            {
+                string url = await new FirebaseStorage("studentorganisati.appspot.com")
+                    .Child("user")
+                    .Child(user.Id)
+                    .Child("profile.jpg")
+                    .GetDownloadUrlAsync();
+                return url;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+        }
+    }
+}
