ce4bc8c04042d6222f7f1830e5e95ea510d0ad46
Adrian Margineanu <a.margineanu98@gmail.com>
2021-05-10 18:49:31 +0300

    Added colors model  in db

---
+++ StudentOrganisation/StudentOrganisation/Models/ColorsModel.cs
@@ -0,0 +1,15 @@
+﻿using Plugin.CloudFirestore.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentOrganisation.Models
+{
+    public class ColorsModel
+    {
+        [MapTo("title")]
+        public string Title { get; set; }
+        [MapTo("color")]
+        public string Color { get; set; }
+    }
+}
