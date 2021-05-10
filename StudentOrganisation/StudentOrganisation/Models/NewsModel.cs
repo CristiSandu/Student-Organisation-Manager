fd6ef364ff96932d5384fb67119106ecf9d32d7a
Adrian Margineanu <a.margineanu98@gmail.com>
2021-05-10 18:00:24 +0300

    Map the model to fileds in firebase document

--- StudentOrganisation/StudentOrganisation/Models/NewsModel.cs
+++ StudentOrganisation/StudentOrganisation/Models/NewsModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Plugin.CloudFirestore.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,9 +7,13 @@ namespace StudentOrganisation.Models
 {
     class NewsModel
     {
+        [MapTo("title")]
         public string Title { get; set; }
+        [MapTo("content")]
         public string Content { get; set; }
+        [MapTo("description")]
         public string Description { get; set; }
+        [MapTo("date")]
         public DateTime Date { get; set; }
     }
 }
