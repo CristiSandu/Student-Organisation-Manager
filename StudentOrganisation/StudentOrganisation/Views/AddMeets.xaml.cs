baa30adbc5eca00929ffebde7a399e4026392b13
Mihai-Daniel RADU <mihai_daniel.radu99@stud.acs.upb.ro>
2021-05-19 12:25:15 +0300

    Added functionality for MeetsAdd buttons.

--- StudentOrganisation/StudentOrganisation/Views/AddMeets.xaml.cs
+++ StudentOrganisation/StudentOrganisation/Views/AddMeets.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StudentOrganisation.Models;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -12,9 +13,23 @@ namespace StudentOrganisation.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AddMeet : ContentPage
     {
+        MeetsModel _meets = new MeetsModel();
         public AddMeet()
         {
             InitializeComponent();
+            _meets.Date = DateTime.Now;
+            BindingContext = _meets;
+        }
+
+        private async void SaveBtn_Clicked(object sender, EventArgs e)
+        {
+            await Services.MeetsProvider.Create(_meets);
+            await Navigation.PopAsync();
+        }
+
+        private async void CancelBtn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
         }
     }
 }
